6a8f8945d138595a7efecaef4a06d1378601cc71
Ed <edward5445@gmail.com>
2026-02-03 01:31:47 +0200

    Frontend: removed unused file for linter

--- frontend/src/components/common/MarginContainer.tsx
+++
@@ -1,8 +0,0 @@
-const MarginContainer = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-10 xl:px-12">
-      {children}
-    </div>
-  );
-};
-export default MarginContainer;
