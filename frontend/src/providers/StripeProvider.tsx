6ed2333575b0c8645f1b28b304b1e847d60efd8c
Ed <edward5445@gmail.com>
2026-02-16 03:30:32 +0200

    Frontend and Backend: Added complete payment system with stripe pt.8

--- frontend/src/providers/StripeProvider.tsx
+++ frontend/src/providers/StripeProvider.tsx
@@ -3,7 +3,9 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!,
+);
 
 export default function StripeProvider({
   children,
